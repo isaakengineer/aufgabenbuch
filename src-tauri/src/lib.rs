e7952638c8ed963977900abf456f7aa234b9a206
Isaak Engineer <i-engineer@tutamail.com>
2024-09-19 00:25:26 +0200

    erster Tauri versuch
    - Tauri heruntergeladen
    - eine File gewählt

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,69 @@
+use tauri::{AppHandle, Emitter};
+use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+
+use std::path::PathBuf;
+
+#[tauri::command]
+async fn greet(app: AppHandle, name: String) -> String {
+    let result = app.dialog().file().pick_file(move |file_path| {
+        if let Some(file_path) = file_path {
+            println!("Selected file: {}", file_path.to_string());
+        } else {
+            println!("No file selected");
+        }
+    });
+
+    // return a default value or an error message
+    "File selection result".to_string()
+}
+// fn greet(app: AppHandle, name: String) -> String {
+//     // let file_path = app.dialog().file().blocking_pick_file();
+
+//     // format!("Hello, {}! You've been greeted from Rust!", name);
+//     // format!("{} has selected the file", name)
+
+//     // app.dialog()
+//     //     .message("Tauri is Awesome")
+//     //     .kind(MessageDialogKind::Info)
+//     //     .title("Information")
+//     //     .ok_button_label("Absolutely")
+//     //     .show(|result| match result {
+//     //         true => (), // do something,
+//     //         false => () // do something,
+//     //     });
+//     // let message = app.dialog().file().pick_file(move |file_path| {
+//     //     match file_path {
+//     //         None => "No file selected".to_string(),
+//     //         Some(file_path) => "hi there".to_string(),
+//     //     }
+//     // });
+//     let message = {
+//         let rt = Runtime::new().unwrap();
+//         let result = rt.block_on(async {
+//             app.dialog().file().pick_file(move |file_path| {
+//                 match file_path {
+//                     None => "No file selected".to_string(),
+//                     Some(file_path) => "hi there".to_string(),
+//                 }
+//             }).await
+//         });
+
+//         result.unwrap_or("Error selecting file".to_string())
+//     };
+
+//     message
+// }
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
