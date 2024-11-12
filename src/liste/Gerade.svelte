21a39907328602b2e87cfdc3bf3a81fc60f35723
Isaak Engineer <i-engineer@tutamail.com>
2024-11-12 14:44:40 +0100

    letzte Veränderung Teil 3

--- src/liste/Gerade.svelte
+++ src/liste/Gerade.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { format } from "date-fns";
+
 	import { invoke } from "@tauri-apps/api/core";
-	import { format } from "date-fns"; // Assuming date-fns is the internal library for date formatting
+
+	import i18n from '$lib/i18n';
 
 	import { liste, aufgabeAendern } from "./store.js";
 	import { Aufgabe } from "../aufgabe/store.js";
@@ -12,8 +15,6 @@
 	} from "phosphor-svelte";
 	import Circle from 'phosphor-svelte/lib/Circle';
 
-
-
 	let aufgaben = $liste;
 
 	let restVerbergen = true;
@@ -23,7 +24,6 @@
 			(aufgabe) => aufgabe.wochentag === 0,
 		);
 	}
-
 	function heute(aufgaben) {
 		const currentDate = new Date();
 		const currentWeekday = (currentDate.getDay() + 6) % 7 + 1; // damit Montag der Tag mit 1 ist usw.
@@ -41,54 +41,6 @@
 				aufgabe.wochentag != 0,
 		);
 	}
-
-	// Add a new Aufgabe to the liste
-	function addAufgabe(aufgabe) {
-		liste.update((n) => [...n, aufgabe]);
-	}
-
-	// Remove an Aufgabe from the liste by its index
-	function removeAufgabe(index) {
-		liste.update((n) => n.filter((_, i) => i !== index));
-	}
-
-	// Update an existing Aufgabe in the liste
-	function updateAufgabe(index, aufgabe) {
-		liste.update((n) =>
-			n.map((item, i) => (i === index ? aufgabe : item)),
-		);
-	}
-	async function aufgabeGewaelt(aufgabe) {
-		$Aufgabe = aufgabe;
-	}
-
-	const istErledigt = (a) => {
-		return (
-			a.getan !== null ||
-			a.vernachlaessigt !== null ||
-			a.verschoben !== null
-		);
-	};
-	const formatDate = (date) => {
-		return format(new Date(date), "yyyy-MM-dd"); // Format the date as 'day.month.year'
-	};
-	const getHumanReadable = (a) => {
-		let readable = {
-			date: "No date available",
-			status: "No status available",
-		};
-		if (a.getan !== null) {
-			readable.status = "Getan";
-			readable.date = formatDate(a.getan);
-		} else if (a.vernachlaessigt !== null) {
-			readable.status = "Vernachlaessigt";
-			readable.date = formatDate(a.vernachlaessigt);
-		} else if (a.verschoben !== null) {
-			readable.status = "Verschoben";
-			readable.date = formatDate(a.verschoben);
-		}
-		return readable;
-	};
 
 	import { dndzone } from 'svelte-dnd-action';
 	import { flip } from 'svelte/animate';
@@ -123,13 +75,13 @@
 
 {#if $liste.length > 0}
 	<div class="liste">
-		<header>Heute</header>
+		<header>{ $i18n.t('abbild.gerade.header-heute') }</header>
 		{#each heute($liste) as aufgabe}
 			<AufgabeBox aufgabe={aufgabe}/>
 		{/each}
 	</div>
 	<div class="liste">
-		<header>Wochentagunabhängig</header>
+		<header>{ $i18n.t('abbild.gerade.header-unabhängig') }</header>
 		<div use:dndzone={{items, flipDurationMs}} on:consider={handleDndConsider} on:finalize={handleDndFinalize}>
 		{#each items as aufgabe (aufgabe.id)}
 			<div animate:flip={{duration: flipDurationMs}}>
@@ -140,7 +92,7 @@
 	</div>
 	<div class="liste" class:liste-verbergen={restVerbergen}>
 		<header class="komplex">
-			<div class="titel">Andere Tagen</div>
+			<div class="titel">{ $i18n.t('abbild.gerade.header-rest') }</div>
 			{#if andereTagen($liste).length > 0}
 				<a
 					class="ausweiten"
@@ -153,35 +105,16 @@
 			{/if}
 		</header>
 		{#each andereTagen($liste) as aufgabe}
-			<div
-				class="aufgabe"
-				class:erledigt={istErledigt(aufgabe)}
-				class:gewaehlt={ ($Aufgabe.id === aufgabe.id)}
-				on:click={() => aufgabeGewaelt(aufgabe)}
-			>
-				<div class="satz">
-					<div class="id">{aufgabe.id}</div>
-					<div class="extra">
-						{#if aufgabe.notiz}<div class="notiz"><Circle weight="fill" size=".8em" /></div>{/if}
-						{#if aufgabe.link}<div class="link"><Circle weight="fill" size=".8em"/></div>{/if}
-					</div>
-					<div class="beschreibung">{aufgabe.beschreibung}</div>
-				</div>
-			</div>
+			<AufgabeBox aufgabe={aufgabe}/>
 		{/each}
 	</div>
 {:else}
 	<section class="message">
-		<p>
-			Stellen Sie die Priorität ihre Aufgaben an <code
-				>jetzt</code
-			>, um sie hier zu sehen.
-		</p>
+		<p>{@html $i18n.t('abbild.gerade.leer-empfehlung') }</p>
 	</section>
 {/if}
 
 <style lang="scss">
-	@import './aufgabe.scss';
 	@import './liste.scss';
 	.buch {
 		display: flex;
@@ -190,5 +123,10 @@
 			padding: .5rem 0;
 			flex: 1;
 		}
+	}
+	:global(code) {
+		font-family: serif;
+		color: #000;
+		background-color: rgba(0,0,0, .1);
 	}
 </style>
