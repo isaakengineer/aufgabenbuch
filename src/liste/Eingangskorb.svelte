e03b3274a3c4a2561845392849ea5fea05603104
Isaak Engineer <i-engineer@tutamail.com>
2024-11-12 16:06:36 +0100

    Eingangskorb Übersetzung

--- src/liste/Eingangskorb.svelte
+++ src/liste/Eingangskorb.svelte
@@ -1,16 +1,18 @@
 <script>
+	import { invoke } from "@tauri-apps/api/core";
+
+	import i18n from '$lib/i18n';
+
 	import { liste, aufgabeAendern } from "./store.js";
 	import { Aufgabe } from "../aufgabe/store.js";
 	import { Aussehen, Ausstattung } from "../routes/store.js";
 	import Priorisieren from "./Priorisieren.svelte";
+
 	import { CircleDashed, Circle,
 		CaretLineRight,CaretLineLeft
- } from "phosphor-svelte";
-	import { invoke } from "@tauri-apps/api/core";
+ 	} from "phosphor-svelte";
 
-	async function aufgabeGewaelt(aufgabe) {
-		$Aufgabe = aufgabe;
-	}
+
 	let prioritäten = $Aussehen.optionen.prioritaeten;
 	const priorisieren = async (aufgabe, prioritaet) => {
 		console.log(aufgabe, prioritaet);
@@ -25,70 +27,76 @@
 	}
 	let gezeigtePrioritaetName = (pr) => {
 		let p = $Aussehen.optionen.prioritaeten.find((p) => p.id === pr);
-		return p.name;
+		return $i18n.t('priorität-optionen.' + p.id);
 	}
 </script>
 
-<header>
-	{#if $Ausstattung.gezeigtePrioritaet > 0 }
-		<button on:click={() => gezeigtePrioritaetAndern($Ausstattung.gezeigtePrioritaet -1)}>
-			<CaretLineLeft />
-		</button>
-	{/if}
-	<div class="titel">
-		{ gezeigtePrioritaetName($Ausstattung.gezeigtePrioritaet)	}
-	</div>
-	{#if $Ausstattung.gezeigtePrioritaet < 5 }
-		<button on:click={() => gezeigtePrioritaetAndern($Ausstattung.gezeigtePrioritaet +1)}>
-			<CaretLineRight />
-		</button>
-	{/if}
-</header>
-<div class="aufgabentabelle">
-	{#if $liste.length > 0}
-		<div class="tabelle-kopf">
-			<div>id</div>
-			<div>Beschreibung</div>
-			{#if prioritäten.length > 0}
-				{#each prioritäten as p}
-					<div class="prioritaet-auswahl">
-						{ p.name }
+<div class="abbild">
+	<header>
+		{#if $Ausstattung.gezeigtePrioritaet > 0 }
+			<button on:click={() => gezeigtePrioritaetAndern($Ausstattung.gezeigtePrioritaet -1)}>
+				<CaretLineLeft />
+			</button>
+		{/if}
+		<div class="titel">
+			{ gezeigtePrioritaetName($Ausstattung.gezeigtePrioritaet)	}
+		</div>
+		{#if $Ausstattung.gezeigtePrioritaet < 5 }
+			<button on:click={() => gezeigtePrioritaetAndern($Ausstattung.gezeigtePrioritaet +1)}>
+				<CaretLineRight />
+			</button>
+		{/if}
+	</header>
+	<div class="aufgabentabelle">
+		{#if $liste.length > 0}
+			<div class="tabelle-kopf">
+				<div>{ $i18n.t('aufgabe.id') }</div>
+				<div>{ $i18n.t('aufgabe.beschreibung') }</div>
+				{#if prioritäten.length > 0}
+					{#each prioritäten as p}
+						<div class="prioritaet-auswahl">{ $i18n.t('priorität-optionen.' + p.id) }</div>
+					{/each}
+				{/if}
+			</div>
+
+			<div class="tabelle-daten">
+				{#each $liste as aufgabe}
+					<div
+						class="aufgabe"
+						class:gewaehlt={ ($Aufgabe.id === aufgabe.id)}
+						on:click={() => $Aufgabe = aufgabe}
+					>
+						<div class="id">{ aufgabe.id }</div>
+						<div class="satz">{ aufgabe.beschreibung }</div>
+						{#if prioritäten.length > 0}
+							{#each prioritäten as p}
+								<div class="prioritaet-auswahl">
+									{#if aufgabe.prioritaet !== p.id }
+										<button class="p" on:click={() => priorisieren(aufgabe, p)}>
+											<CircleDashed />
+										</button>
+									{:else}
+										<button class="p" on:click={() => priorisieren(aufgabe, p)}>
+											<Circle weight="fill" />
+										</button>
+									{/if}
+								</div>
+							{/each}
+						{/if}
 					</div>
 				{/each}
-			{/if}
-		</div>
-
-		<div class="tabelle-daten">
-			{#each $liste as aufgabe}
-				<div
-					class="aufgabe"
-					class:gewaehlt={ ($Aufgabe.id === aufgabe.id)}
-					on:click={() => aufgabeGewaelt(aufgabe)}
-				>
-					<div class="id">{ aufgabe.id }</div>
-					<div class="satz">{ aufgabe.beschreibung }</div>
-					{#if prioritäten.length > 0}
-						{#each prioritäten as p}
-							<div class="prioritaet-auswahl">
-								{#if aufgabe.prioritaet !== p.id }
-									<button class="p" on:click={() => priorisieren(aufgabe, p)}>
-										<CircleDashed />
-									</button>
-								{:else}
-									<button class="p" on:click={() => priorisieren(aufgabe, p)}>
-										<Circle weight="fill" />
-									</button>
-								{/if}
-							</div>
-						{/each}
-					{/if}
-				</div>
-			{/each}
-		</div>
-	{/if}
+			</div>
+		{/if}
+	</div>
 </div>
 
 <style lang='scss'>
+.abbild {
+	max-height: 100%;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+}
 header {
 	padding: 1rem;
 	display: flex;
@@ -100,11 +108,14 @@ header {
 	}
 }
 .aufgabentabelle {
-	background-color: gray;
 	display: flex;
-	gap: 1px;
 	flex-direction: column;
 	grid-template-rows: 1fr 1px 1fr;
+	overflow: hidden;
+	max-height: 100%;
+	> .tabelle-kopf {
+		margin-bottom: 2px;
+	}
 	> .tabelle-kopf, .tabelle-daten > div {
 		display: grid;
 		grid-template-columns: 3rem 1fr repeat(6, 3rem);
@@ -115,8 +126,8 @@ header {
 	}
 	> .tabelle-kopf .prioritaet-auswahl {
 		padding: .5rem .2rem;
-  	writing-mode: vertical-lr;
-   	text-transform: capitalize;
+  		writing-mode: vertical-lr;
+   		text-transform: capitalize;
 	}
 	> .tabelle-kopf > div {
 		text-align: center;
@@ -130,6 +141,7 @@ header {
 		display: flex;
 		flex-direction: column;
 		gap: 1px;
+		overflow-y: auto;
 		> .aufgabe {
 			.satz, .id {
 				padding: .2rem .4rem;
